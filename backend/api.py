2d5c0be69ba3ec86b8cf5bc8fb5dc255c527f0eb
sanmaykant <sanmaykant1@gmail.com>
2025-04-06 00:42:58 +0530

    Using dummy data to improve frontend

--- backend/api.py
+++ backend/api.py
@@ -1,16 +1,25 @@
+import json
+
 from flask import Flask, jsonify
 from flask_cors import CORS
 
 from analyze import analyze_company
+from utils import validate_response
 
 app = Flask(__name__)
 CORS(app)
 
 @app.route('/company/<name>', methods=['GET'])
 def greet(name):
-    result = analyze_company(name)
-    print(result)
-    return jsonify(result)
+    # result = analyze_company(name)
+    # print(result)
+    # return jsonify(result)
+
+    with open("test.json", "r") as file:
+        content = file.read()
+        python_content = json.loads(content)
+        response = validate_response(python_content)
+        return jsonify(response)
 
 if __name__ == '__main__':
     app.run(debug=True)
