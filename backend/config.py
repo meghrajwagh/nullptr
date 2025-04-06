ff82509dccea32f77b1ee8a329f61636f03df10b
sanmaykant <sanmaykant1@gmail.com>
2025-04-06 07:45:09 +0530

    Final config updated

--- backend/config.py
+++ backend/config.py
@@ -1,14 +1,12 @@
 NUMBER_OF_RESULTS_PER_SOURCE = 2
 
 media_houses = [
-    "https://www.reuters.com/",
     "https://www.ndtv.com/",
     "https://www.thehindu.com/",
     "https://www.bbc.co.uk/",
 ]
 
 financial_media_houses = [
-    "https://economictimes.indiatimes.com/",
     "https://www.bloomberg.com/businessweek",
     "https://www.ft.com/",
     "https://www.cnbc.com/world/?region=world"
